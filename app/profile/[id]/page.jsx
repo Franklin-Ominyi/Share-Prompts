b859043dd3adc58410db0608710e156101ed3cde
Franklin-Ominyi <franklinchibuikeominyi@gmail.com>
2023-07-28 07:30:08 +0100

    Fixing import errors

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const AboutProfile = ({ params }) => {
 	const { id } = params;
